3ebcb9abf00940e4918db5cb21a982f0c36083b6
Wendy-Tabitha <wendytbt4@gmail.com>
2025-02-10 16:45:49 +0300

    fix: change output formatting

--- utils/startdownload.go
+++ utils/startdownload.go
@@ -10,7 +10,7 @@ import (
 
 func StartDownload(url, fileName string, background bool) {
 	startTime := time.Now()
-	fmt.Printf("start at %s\n", startTime.Format("2006-01-02 15:04:05"))
+	fmt.Printf("--%s--  %s\n", startTime.Format("2006-01-02 15:04:05"), url)
 
 	// Send request
 	resp, err := http.Get(url)
@@ -22,13 +22,16 @@ func StartDownload(url, fileName string, background bool) {
 
 	// Check response status
 	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Status: %d %s\n", resp.StatusCode, http.StatusText(resp.StatusCode))
+		fmt.Printf("HTTP request sent, awaiting response... %d %s\n", resp.StatusCode, http.StatusText(resp.StatusCode))
 		return
 	}
 
 	// Get content length
 	contentLength := resp.ContentLength
-	fmt.Printf("content size: %d [~%.2fMB]\n", contentLength, float64(contentLength)/1024/1024)
+	if contentLength < 0 {
+		contentLength = 0 // Handle unknown content length
+	}
+	fmt.Printf("Length: %d (%s) [%s]\n", contentLength, formatSize(contentLength), resp.Header.Get("Content-Type"))
 
 	// Create file
 	out, err := os.Create(fileName)
@@ -46,8 +49,8 @@ func StartDownload(url, fileName string, background bool) {
 		return
 	}
 
-	fmt.Printf("Downloaded [%s]\n", url)
-	fmt.Printf("finished at %s\n", time.Now().Format("2006-01-02 15:04:05"))
+	fmt.Printf("\n%s 100%%[========================>]  %s   in %s\n", fileName, formatSize(contentLength), time.Since(startTime))
+	fmt.Printf("'%s' saved [%d/%d]\n", fileName, contentLength, contentLength)
 }
 
 func (pw *ProgressWriter) Write(p []byte) (n int, err error) {
@@ -59,6 +62,15 @@ func (pw *ProgressWriter) Write(p []byte) (n int, err error) {
 	elapsed := time.Since(pw.startTime)
 	remaining := time.Duration(float64(elapsed) / (float64(pw.current) / float64(n)) * float64(pw.total-pw.current))
 
-	fmt.Printf("\r %d / %d [%.2f%%] %s remaining", pw.current, pw.total, percentage, remaining)
+	fmt.Printf("\r %s %d / %d [%.2f%%] %s remaining", formatSize(pw.current), pw.current, pw.total, percentage, remaining)
 	return n, nil
+}
+
+func formatSize(size int64) string {
+	if size >= 1_048_576 { // 1 MiB
+		return fmt.Sprintf("%.2fMiB", float64(size)/1_048_576)
+	} else if size >= 1_024 { // 1 KiB
+		return fmt.Sprintf("%.2fKiB", float64(size)/1_024)
+	}
+	return fmt.Sprintf("%dB", size)
 }
